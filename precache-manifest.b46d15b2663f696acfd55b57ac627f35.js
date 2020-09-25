d590ea70f4a810c229adc6a23e0711edd5278ac7
Bharat <bharat@kali.com>
2020-09-25 20:05:13 +0530

    Updates

---
+++ precache-manifest.b46d15b2663f696acfd55b57ac627f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78de0f12b9cc6399347faf11be9fb60",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "af959bf430bcc5bdfcbf",
+    "url": "/monster-rolodex/static/css/main.909cc030.chunk.css"
+  },
+  {
+    "revision": "c284f5b63d4ca25b82a2",
+    "url": "/monster-rolodex/static/js/2.e770947b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.e770947b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af959bf430bcc5bdfcbf",
+    "url": "/monster-rolodex/static/js/main.93576642.chunk.js"
+  },
+  {
+    "revision": "170a0493b1d1d2505b6b",
+    "url": "/monster-rolodex/static/js/runtime-main.a99ff6d4.js"
+  }
+]);
